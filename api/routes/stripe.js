3a5a5cf54a057bc6ad7aaa598921ee626fd1688d
AyushToshniwal <ayush.toshniwal.1302@gmail.com>
2023-07-28 14:32:42 +0530

    First Commit

---
+++ api/routes/stripe.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+// const stripe = require("stripe")(process.env.STRIPE_KEY);
+const KEY = process.env.STRIPE_KEY
+const stripe = require("stripe")(KEY);
+
+router.post("/payment", (req, res) => {
+  stripe.charges.create(
+    {
+      source: req.body.tokenId, //stripe will return a token id and it will be used here
+      amount: req.body.amount,
+      currency: "usd",
+    },
+    (stripeErr, stripeRes) => {
+      if (stripeErr) {
+        res.status(500).json(stripeErr);
+      } else {
+        res.status(200).json(stripeRes);
+      }
+    }
+  );
+});
+
+module.exports = router;
